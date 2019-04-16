43d11871f502c064500bec405f2dda4b73100702
Nate Barlow <nathanbarlow23@gmail.com>
2019-04-16 12:27:13 -0600

    work on pages

---
+++ src/views/Categories.vue
@@ -0,0 +1,157 @@
+<template>
+  <div>
+    <h1 class="pt-4 px-5">Manage Your Spending Categories</h1>
+    <div class="container-fluid pb-5">
+      <div class="table-responsive mt-5">
+        <table class="table table-hover text-center">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Category Name</th>
+              <th scope="col">Description</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-bind:key="category._id" v-for="category in categories">
+              <th scope="row">{{categoryNum(category)}}</th>
+              <td>{{category.title}}</td>
+              <td>{{category.description}}</td>
+              <td>
+                <button type="button" class="btn btn-info" v-on:click="edit(category)">Edit</button>
+                <button
+                  type="button"
+                  class="btn btn-danger"
+                  v-on:click="deleteCategory(category)"
+                >Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="container my-3">
+        <div class="row">
+          <button
+            id="button"
+            v-on:click="onClickNewCategory"
+            type="button"
+            class="btn bg-prim center"
+          >
+            New
+            Category
+          </button>
+          <form class="m-auto" id="form" v-on:submit.prevent="createCategory">
+            <input v-model="addedCategoryName" placeholder="Category">
+            <input v-model="addedDescription" placeholder="Description">
+            <button type="submit">Submit</button>
+          </form>
+          <form class="m-auto" id="editForm" v-on:submit.prevent="updateCategory">
+            <input v-model="editCategoryName" placeholder="Category">
+            <input v-model="editDescription" placeholder="Description">
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      categories: [],
+      addedCategoryName: "",
+      addedDescription: "",
+      editCategoryName: "",
+      editDescription: "",
+      editCategoryId: "",
+      showForm: false,
+      showEditForm: false,
+      showButton: true
+    };
+  },
+  created() {
+    this.getCategories();
+  },
+  methods: {
+    async getCategories() {
+      try {
+        let response = await axios.get("/api/categories");
+        this.categories = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    onClickNewCategory() {
+      this.showForm();
+    },
+    showForm() {
+      this.showButton = false;
+      this.showForm = true;
+    },
+    showEditForm() {
+      this.showButton = false;
+      this.showEditForm = true;
+      this.showForm = false;
+    },
+    showButton() {
+      this.showButton = true;
+      this.showEditForm = false;
+      this.showForm = false;
+    },
+    async createCategory() {
+      try {
+        await axios.post("/api/categories", {
+          title: this.addedCategoryName,
+          description: this.addedDescription
+        });
+        this.addedCategoryName = "";
+        this.addedDescription = "";
+        this.showButton();
+        this.getCategories();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async deleteCategory(category) {
+      try {
+        let response = await axios.delete("/api/categories/" + category._id);
+        this.getCategories();
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    categoryNum(category) {
+      return this.categories.indexOf(category) + 1;
+    },
+    edit(category) {
+      this.editCategoryName = category.title;
+      this.editDescription = category.description;
+      this.editCategoryId = category._id;
+      this.showEditForm();
+    },
+    async updateCategory() {
+      try {
+        const response = await axios.put(
+          "/api/categories/" + this.editCategoryId,
+          {
+            title: this.editCategoryName,
+            description: this.editDescription
+          }
+        );
+        this.getCategories();
+        this.showButton();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
