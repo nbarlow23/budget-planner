ef3b635dc260101132ba3774dfa7179af9608fb5
ClintonMcCardell <mccardell.clinton@gmail.com>
2019-04-16 11:01:59 -0600

    added action logic

--- src/store.js
+++ src/store.js
@@ -26,6 +26,41 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    
+    async register(context, data) {
+      try {
+        let response = await axios.post("/api/users", data);
+        context.commit("setUser", response.data);
+        return "";
+      } catch (error) {
+        return error.response.data.message;
+      }
+    },
+    async login(context, data) {
+      try {
+        let response = await axios.post("/api/users/login", data);
+        context.commit("setUser", response.data);
+        return "";
+      } catch (error) {
+        return error.response.data.message;
+      }
+    },
+    async logout(context) {
+      try {
+        await axios.delete("/api/users");
+        context.commit("setUser", null);
+        return "";
+      } catch (error) {
+        return error.response.data.message;
+      }
+    },
+    async getUser(context) {
+      try {
+        let response = await axios.get("/api/users");
+        context.commit("setUser", response.data);
+        return "";
+      } catch (error) {
+        return "";
+      }
+    }
   }
 });
